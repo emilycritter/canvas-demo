1c18ef603e0f4153bab70ccbc173254e0ad99560
Emily Ritter <ritter@gcgmarketing.com>
2019-08-27 17:15:24 -0500

    Adds mesh and lights to threejs demo

--- threejs/js/main.js
+++ threejs/js/main.js
@@ -13,16 +13,14 @@ document.addEventListener("DOMContentLoaded", function(){
   const container =
       document.querySelector('#container');
 
-  // Create a WebGL renderer, camera
-  // and a scene
+  // Create a WebGL renderer, camera, and a scene
   const renderer = new THREE.WebGLRenderer();
-  const camera =
-      new THREE.PerspectiveCamera(
-          VIEW_ANGLE,
-          ASPECT,
-          NEAR,
-          FAR
-      );
+  const camera = new THREE.PerspectiveCamera(
+    VIEW_ANGLE,
+    ASPECT,
+    NEAR,
+    FAR
+  );
 
   const scene = new THREE.Scene();
 
@@ -32,7 +30,45 @@ document.addEventListener("DOMContentLoaded", function(){
   // Start the renderer.
   renderer.setSize(WIDTH, HEIGHT);
 
-  // Attach the renderer-supplied
-  // DOM element.
+  // Attach the renderer-supplied DOM element.
   container.appendChild(renderer.domElement);
+
+  // Set up the sphere vars
+  const RADIUS = 50;
+  const SEGMENTS = 16;
+  const RINGS = 16;
+
+  // Create the sphere's sphere
+  const sphereMaterial = new THREE.MeshLambertMaterial({
+    color:  0xCC0000
+  });
+
+  // Create a new mesh with sphere geometry
+  const sphere = new THREE.Mesh(
+    new THREE.SphereGeometry(
+      RADIUS,
+      SEGMENTS,
+      RINGS
+    ),
+    sphereMaterial
+  );
+
+  // Move the Sphere back in Z so we can see it
+  sphere.position.z = -300;
+
+  // Add the sphere to the scene
+  scene.add(sphere);
+
+  // Create a point light
+  const pointLight = new THREE.PointLight(0xFFFFFF);
+
+  // Set the light position
+  pointLight.position.x = 10;
+  pointLight.position.y = 50;
+  pointLight.position.z = 130;
+
+  // Add the light to the scene
+  scene.add(pointLight);
+
+  renderer.render(scene, camera);
 });
